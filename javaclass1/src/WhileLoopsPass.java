be84e0d62d0553fc354a01b8d502cdcc5dad18a7
kkhairul <45337360+kkhairul@users.noreply.github.com>
2021-08-04 15:45:47 -0400

    Update WhileLoopsPass.java

--- javaclass1/src/WhileLoopsPass.java
+++ javaclass1/src/WhileLoopsPass.java
@@ -2,16 +2,7 @@ import java.util.Scanner;
 
 public class WhileLoopsPass {
     public static void main (String [] args){
-     /*
-     *
-     * initialization
-     * comparison
-     * update
-     *
-      */
-       // int i = 1;
-        //while (i <10 ){
-          //  System.out.println("hello world"); i++;
+   
         System.out.println("Guess the password:");
         String password = "let me in";
         Scanner scanner = new Scanner(System.in);
