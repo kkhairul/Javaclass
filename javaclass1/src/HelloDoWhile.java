5f2eadc6d0ef28ed97599c545539c5ac323c15b4
kkhairul <45337360+kkhairul@users.noreply.github.com>
2021-08-04 15:29:49 -0400

    Add files via upload

---
+++ javaclass1/src/HelloDoWhile.java
@@ -0,0 +1,8 @@
+public class HelloDoWhile {
+    public static void main (String [] args){
+        int i = 1;{
+          do {
+            System.out.println("hello" + 1);
+          i++;
+        } while (i <=3);
+}}}
