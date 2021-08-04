cc5b021299f06da065c767d12961fec3093c2800
kkhairul <45337360+kkhairul@users.noreply.github.com>
2021-08-04 15:30:02 -0400

    Add files via upload

---
+++ javaclass1/src/IfStatement.java
@@ -0,0 +1,14 @@
+public class IfStatement {
+    public static void main ( String [] args){
+    int x = 5; {
+        if (x<10){
+            System.out.println("A");
+        }
+        if (x<4){
+            System.out.println("B");
+        }
+        if ( x<7){
+            System.out.println("C");
+        }
+    }
+}}
