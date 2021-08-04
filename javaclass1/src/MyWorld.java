faba27bf7f769f0f07382a23a8260aee2d4bcf63
kkhairul <45337360+kkhairul@users.noreply.github.com>
2021-08-04 15:30:42 -0400

    Add files via upload

---
+++ javaclass1/src/MyWorld.java
@@ -0,0 +1,13 @@
+public class MyWorld {
+
+        public static void main (String[] args)
+        {
+            myWorld();
+
+        }
+        public static void myWorld(){
+            for (int i = 0; i < 5; i ++){
+                System.out.println("Hello World");
+            }
+        }
+}
