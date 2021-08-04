35476429c2121e522de545c2bb4f597de2264770
kkhairul <45337360+kkhairul@users.noreply.github.com>
2021-08-04 15:42:02 -0400

    Add files via upload

---
+++ javaclass1/src/Expiration.java
@@ -0,0 +1,24 @@
+public class Expiration
+{
+
+
+    public static void main(String[] args) {
+        Expi(8/9/2021);
+
+
+    }
+    public static void Expi(int Expi ) {
+
+
+        if (Expi< 8/9/2021) {
+
+            System.out.println("good food ");
+        }
+        else if (Expi > 8/9/2021) {
+            System.out.println("bad food ");
+
+
+        }
+    }
+}
+
