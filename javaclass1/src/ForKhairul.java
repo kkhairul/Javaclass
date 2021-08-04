0a913b30dbb26ec9e7db8eb0e1d6967f4d116a12
kkhairul <45337360+kkhairul@users.noreply.github.com>
2021-08-04 15:29:03 -0400

    Add files via upload

---
+++ javaclass1/src/ForKhairul.java
@@ -0,0 +1,15 @@
+public class ForKhairul
+{
+
+        public static void main (String[] args) {
+
+
+
+
+
+            for (int i = 1; i < 2; i ++){
+                System.out.println(" khairul #" +i);
+
+            }}}
+
+
