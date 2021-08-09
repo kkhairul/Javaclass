7d6da579543031755fab50817d37272208be9176
kkhairul <45337360+kkhairul@users.noreply.github.com>
2021-08-09 18:36:43 -0400

    Add files via upload

---
+++ javaclass1/src/Rectangle.java
@@ -0,0 +1,51 @@
+
+public class Rectangle {
+
+    int length;
+    int breadth;
+
+
+
+
+    Rectangle(int l, int b) {//2
+        length = l;
+        breadth = b;
+    }
+
+    public void area (){
+        int ans = length * breadth;
+        System.out.println("Area of Recttangle is-  "  + ans) ;
+
+    }
+    public void perameater(){
+        int ans = 2*(length+breadth);
+        System.out.println("PERIMETER IS RECTANGLE- "  +ans);
+
+    }
+
+
+}
+class Square extends Rectangle {
+    public void area (){
+        int ans = length * breadth;
+        System.out.println("Area of Square is-  "  + ans) ;
+
+    }
+
+    public void perameater() {
+        int ans = 4 * (length + breadth);
+        System.out.println("PERIMETER IS Square- " + ans);
+    }
+    Square(){
+        super(8, 9);
+
+    }
+    public static void main (String [] args) {
+        Rectangle r = new Rectangle(5, 6);
+        r.area();
+        r.perameater();
+        Square s = new Square();
+        s.area();
+        s.perameater();//no
+
+    }}
