80de325aeb3f1bc3a6b213a7567d5b5a1b65215b
kkhairul <45337360+kkhairul@users.noreply.github.com>
2021-08-11 18:24:18 -0400

    Add files via upload

---
+++ javaclass1/src/exception/ForLoopCatch.java
@@ -0,0 +1,22 @@
+
+package exception;
+import java.util.*;
+public class ForLoopCatch {
+    public static void main(String[] args) {
+
+
+    int[] numer = {4, 8, 16, 32};
+    int[] denom = {2, 0, 4, 4};
+
+    for(int i = 0; i<numer.length;i ++)
+
+    {
+        try {
+            System.out.println(numer[i] / denom[i]);
+        } catch (ArithmeticException ex) {
+            System.out.println("cat't divide by zero ");
+        }
+    }
+}
+
+}
